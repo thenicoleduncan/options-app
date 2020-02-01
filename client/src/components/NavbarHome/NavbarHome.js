39916344a6a0bbcb0b1538f969bf345cf1270ee4
Brooke <parrish.e.brooke@gmail.com>
2020-02-01 11:49:10 -0700

    added to navbar and footer

---
+++ client/src/components/NavbarHome/NavbarHome.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { NavLink } from 'react-router-dom';
+import './NavbarHome.css';
+
+function NavbarHome() {
+    return (
+        <Navbar className="pt-5 navnav" variant="dark">
+            <Nav className="mr-auto">
+                <NavLink className="pr-2" to="/">home</NavLink>
+                <NavLink className="pr-2" to="/Blog">blog</NavLink>
+                <NavLink className="pr-2" to="/Resources">resources</NavLink>
+                <NavLink className="pr-2" to="/FindOpportunities">find opportunities</NavLink>
+                <NavLink className="pr-2" to="/Login">Login</NavLink>
+                <NavLink className="pr-2" to="/SignUp">SignUp</NavLink>
+            </Nav>
+        </Navbar>
+    );
+}
+
+export default NavbarHome;
