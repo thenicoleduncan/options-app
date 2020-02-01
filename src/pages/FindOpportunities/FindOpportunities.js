c5f4dd0409562eb31376e9800cd51d0de2199031
Brooke <parrish.e.brooke@gmail.com>
2020-02-01 11:11:23 -0700

    added pages and componenets

---
+++ src/pages/FindOpportunities/FindOpportunities.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class FindOpportunities extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Find Opportunities</h1>
+      </div>
+    );
+  }
+}
+
+export default FindOpportunities;
