4d27f8af694358c3988b9adbedb45e1a81cf0c10
Joshua Einsel <Joshua.einsel@Live.com>
2020-02-04 18:03:04 -0700

    Fixed Auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-
 import NavbarHome from './components/NavbarHome/NavbarHome';
 import Home from './pages/Home/Home';
 import Blog from './pages/Blog/Blog';
@@ -9,10 +8,17 @@ import Resources from './pages/Resources/Resources';
 import FindOpportunities from './pages/FindOpportunities/FindOpportunities';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
+import { useAuth0 } from "./react-auth0-spa";
 
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
+
     <Router>
       <div className="App">
         <p>Hello World</p>
--- client/src/components/NavbarHome/NavbarHome.js
+++ client/src/components/NavbarHome/NavbarHome.js
@@ -3,19 +3,33 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
 import './NavbarHome.css';
+import { useAuth0 } from "../../react-auth0-spa";
+
 
 function NavbarHome() {
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+
+
     return (
-        <Navbar className="pt-5 navnav" variant="dark">
-            <Nav className="mr-auto">
-                <NavLink className="pr-2" to="/">home </NavLink>
-                <NavLink className="pr-2" to="/Blog">blog </NavLink>
-                <NavLink className="pr-2" to="/Resources">resources </NavLink>
-                <NavLink className="pr-2" to="/FindOpportunities">find opportunities </NavLink>
-                <NavLink className="pr-2" to="/Login">login </NavLink>
-                <NavLink className="pr-2" to="/SignUp">signup</NavLink>
-            </Nav>
-        </Navbar>
+        <div>
+
+            <Navbar className="pt-5 navnav" variant="dark">
+                <Nav className="mr-auto">
+                    <NavLink className="pr-2" to="/">home</NavLink>
+                    <NavLink className="pr-2" to="/Blog">blog</NavLink>
+                    <NavLink className="pr-2" to="/Resources">resources</NavLink>
+                    <NavLink className="pr-2" to="/FindOpportunities">find opportunities</NavLink>
+                </Nav>
+            </Navbar>
+            <div>
+                {!isAuthenticated && (
+                    <button onClick={() => loginWithRedirect({})}>Log in</button>
+                )}
+
+                {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+            </div>
+        </div>
     );
 }
 
