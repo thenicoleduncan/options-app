10c6c896ebfcb01603bbe0d284d7a0151b32dade
Joshua Einsel <Joshua.einsel@Live.com>
2020-02-08 10:05:13 -0700

    Added authentication based resource button

--- client/src/components/AddResourceBtn/AddResourceBtn.js
+++ client/src/components/AddResourceBtn/AddResourceBtn.js
@@ -1,16 +1,22 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 import './AddResourceBtn.css';
+import { useAuth0 } from "../../react-auth0-spa";
+
 
 
 function AddResourceBtn() {
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
     return (
-
         <div>
-            <button>Add a Resource</button>
-        </div>
+            {!isAuthenticated && (
+                <button onClick={() => loginWithRedirect({})}>Log in to add a resource</button>
+            )}
 
+            {isAuthenticated && <button>Add a Resource</button>}
+
+        </div>
     );
 }
 
